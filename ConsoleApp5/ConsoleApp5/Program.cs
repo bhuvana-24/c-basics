58db77d4ebe67471d4366f24865dbad3b65b5aa1
bhuvana-24 <b.ravulla@gmail.com>
2019-01-20 00:55:10 +0530

    modulepng

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            simpleInterest interest = new simpleInterest(23,908,4.5);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/simpleInterest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class simpleInterest
+    {
+        double prin, rate, time, i, a;
+        public simpleInterest(double aprin, double arate ,double atime )
+        {
+            prin = aprin;
+            rate = arate;
+            time = atime;
+
+            i = (prin * rate * time) / 100;
+            Console.WriteLine(i);
+            a = (prin+ i);
+            Console.WriteLine (a);
+            
+        }
+
+
+    }
+}
