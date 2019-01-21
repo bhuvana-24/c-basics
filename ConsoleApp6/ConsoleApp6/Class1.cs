43d2613fd95a0a2e2882315dac83e06447ba288b
bhuvana-24 <b.ravulla@gmail.com>
2019-01-21 21:36:39 +0530

    moduleprng

---
+++ ConsoleApp6/ConsoleApp6/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    class calculateInterest
+    {
+        public double principal;
+        public double rate;
+        public double time;
+        public void calculate()
+        {
+            Console.WriteLine("Enter principal: ");
+            principal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter rate: ");
+            rate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter time: ");
+            time = Convert.ToDouble(Console.ReadLine());
+
+        }
+            public double GetInterest()
+        {
+            return principal*rate*time;
+        }
+        public void Display()
+        {
+            Console.WriteLine("interest: {0}", GetInterest());
+        }
+    }
+
+
+
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+
+    class simpleInterest
+    {
+        static void Main(string[] args)
+        {
+            calculateInterest i = new calculateInterest();
+            i.calculate();
+            i.Display();
+            Console.ReadLine();
+        }
+    }
+}    
+
+
+
+
+
